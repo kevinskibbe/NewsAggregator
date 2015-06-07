10373e7f2d71e63818d06e9bc2b14f56794fdbb1
KevinSkibbe <kevin.skibbe@bettercloud.com>
2015-06-07 19:18:24 -0400

    branding and google analytics changes

--- NewsAggregator.UI/app/components/home/articledirective.js
+++ NewsAggregator.UI/app/components/home/articledirective.js
@@ -1,7 +1,7 @@
 app.directive('articleDirective', function($sce) {
     return {
         templateUrl: function () {
-            return $sce.trustAsResourceUrl('http://kevinskibbe-001-site2.smarterasp.net/app/components/home/article-section.html');
+            return $sce.trustAsResourceUrl('/app/components/home/article-section.html');
 		}
 	};
 });
